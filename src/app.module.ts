f1a29141e550a7f496945817b5d56bddc9706906
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2026-03-17 17:40:53 +0700

    setup project structure and start auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ErrorHandlingMiddleware } from './common/filters/middleware/error-handling.middleware';
+import { PinoLogger } from './utils/logger/pino.logger';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: Logger,
+      useClass: PinoLogger,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ErrorHandlingMiddleware) // Đăng ký middleware xử lý lỗi
+      .forRoutes('*'); // Áp dụng cho tất cả các route, có thể thay đổi theo nhu cầu
+  }
+}
