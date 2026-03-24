a418cd4140c81d3a286bc5ab8565e1d66453b638
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2026-03-24 10:13:28 +0700

    api/nhom-chi-tiet-loai-cong-viec/phan-trang-tim-kiem

--- src/modules/nhom-chi-tiet-loai-cong-viec/nhom-chi-tiet-loai-cong-viec.controller.ts
+++ src/modules/nhom-chi-tiet-loai-cong-viec/nhom-chi-tiet-loai-cong-viec.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateNhomChiTietLoaiCongViecDto } from './dtos/create-nhom-chi-tiet-loai-cong-viec.dto';
 import { NhomChiTietLoaiCongViecService } from './nhom-chi-tiet-loai-cong-viec.service';
+import { QueryLoaiCongViecDto } from '../loai-cong-viec/dtos/query-loai-cong-viec.dto';
 
 @Controller('api/nhom-chi-tiet-loai-cong-viec')
 export class NhomChiTietLoaiCongViecController {
@@ -18,5 +19,13 @@ export class NhomChiTietLoaiCongViecController {
   @Get()
   findAll() {
     return this.nhomChiTietLoaiCongViecService.findAll();
+  }
+
+  // GET api/nhom-chi-tiet-loai-cong-viec/phan-trang-tim-kiem?pageIndex=1&pageSize=10&keyword=abc
+  @Get('phan-trang-tim-kiem')
+  findAllPaginationAndSearch(@Query() query: QueryLoaiCongViecDto) {
+    return this.nhomChiTietLoaiCongViecService.findAllPaginationAndSearch(
+      query,
+    );
   }
 }
--- src/modules/nhom-chi-tiet-loai-cong-viec/nhom-chi-tiet-loai-cong-viec.service.ts
+++ src/modules/nhom-chi-tiet-loai-cong-viec/nhom-chi-tiet-loai-cong-viec.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateNhomChiTietLoaiCongViecDto } from './dtos/create-nhom-chi-tiet-loai-cong-viec.dto';
 import { PrismaService } from '../../../prisma/prisma.service';
+import { QueryLoaiCongViecDto } from '../loai-cong-viec/dtos/query-loai-cong-viec.dto';
 
 @Injectable()
 export class NhomChiTietLoaiCongViecService {
@@ -45,6 +46,54 @@ export class NhomChiTietLoaiCongViecService {
     return {
       statusCode: 200,
       content: nhomChiTietLoaiCongViecAll,
+    };
+  }
+
+  async findAllPaginationAndSearch(query: QueryLoaiCongViecDto) {
+    const pageIndex = Number(query.pageIndex) || 1;
+    const pageSize = Number(query.pageSize) || 10;
+    const keyword = query.keyword?.trim() || '';
+
+    if (pageIndex < 1 && pageSize < 1) {
+      throw new BadRequestException(
+        'pageIndex và pageSize phải lớn hơn 1',
+        HttpStatus.BAD_REQUEST.toString(),
+      );
+    }
+
+    const whereCondition = keyword
+      ? {
+          ten_nhom: { contains: keyword },
+        }
+      : {};
+
+    const [data, totalRow] = await Promise.all([
+      this.prisma.nhomChiTietLoaiCongViec.findMany({
+        where: whereCondition,
+        skip: (pageIndex - 1) * pageSize,
+        take: pageSize,
+        orderBy: {
+          id: 'asc',
+        },
+      }),
+
+      this.prisma.nhomChiTietLoaiCongViec.count({
+        where: whereCondition,
+      }),
+    ]);
+
+    return {
+      statusCode: 200,
+      message:
+        'Lấy danh sách nhóm chi tiết loại công việc phân trang, tìm kiếm thành công',
+      content: {
+        pageIndex,
+        pageSize,
+        keyword,
+        totalRow,
+        totalPage: Math.ceil(totalRow / pageSize),
+        data,
+      },
     };
   }
 }
