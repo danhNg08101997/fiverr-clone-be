a97f86a28fa0f0f26463375bbd334971f9d5e53c
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2026-04-01 14:53:47 +0700

    setup deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get('health')
+  health() {
+    return { status: 'ok' };
+  }
+}
