a2876e6ba47e736ed44dae68b2bf58ffb68116c3
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2026-04-02 11:29:57 +0700

    fixbug deploy timeout cause port incorrect

--- src/main.ts
+++ src/main.ts
@@ -72,7 +72,7 @@ async function bootstrap() {
     `,
   });
 
-  const port = Number(process.env.PORT || 10000);
+  const port = Number(process.env.PORT || 3000);
   await app.listen(port, '0.0.0.0');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
